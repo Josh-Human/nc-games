77b5622da580d697dea37a7974d7c967555b0fbf
Josh-Human <jhumanb1@gmail.com>
2022-01-10 15:23:39 +0000

    start navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <>
+            <Link to="/">Icon</Link>
+            <Link to="/">Home</Link>
+            <Link to="/">Categories</Link>
+            <Link to="/">Profile</Link>
+        </>
+    );
+};
+
+export default NavBar;

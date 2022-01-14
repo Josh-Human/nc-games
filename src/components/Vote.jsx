3a84bf138b1de909469070cbdd64666c80b593c6
Josh-Human <jhumanb1@gmail.com>
2022-01-14 16:05:50 +0000

    refactor vote component

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
     patchCommentVote,
     patchReviewVotes,
@@ -7,25 +7,21 @@ import {
 
 const Vote = ({ item, itemStr }) => {
     const [voteChange, setVoteChange] = useState(0);
-
-    const handleVote = ({ target }) => {
-        let inc_vote = 0;
-        if (target.id === `${item[`${itemStr}_id`]}__inc`) {
-            inc_vote = 1;
-        } else {
-            inc_vote = -1;
+    useEffect(() => {
+        if (voteChange !== 0) {
+            patchVotes(voteChange, item[`${itemStr}_id`], itemStr + "s").catch(
+                () => {
+                    voteChange === 1 ? setVoteChange(-1) : setVoteChange(1);
+                }
+            );
         }
-        patchVotes(inc_vote, item[`${itemStr}_id`], itemStr + "s").catch(() => {
-            inc_vote === 1 ? setVoteChange(-1) : setVoteChange(1);
-        });
-    };
+    }, [voteChange, item, itemStr]);
 
     return (
         <div>
             <button
                 id={`${item[`${itemStr}_id`]}__inc`}
                 onClick={(event) => {
-                    handleVote(event);
                     setVoteChange(1);
                 }}
                 disabled={voteChange === 1}
@@ -36,7 +32,6 @@ const Vote = ({ item, itemStr }) => {
             <button
                 id={`${item[`${itemStr}_id`]}__dec`}
                 onClick={(event) => {
-                    handleVote(event);
                     setVoteChange(-1);
                 }}
                 disabled={voteChange === -1}
